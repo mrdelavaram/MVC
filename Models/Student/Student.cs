7817b50974e869d350b2247ccc32198e70b86496
mrdelavaram <mohammaddel80@gmail.com>
2024-01-07 20:21:31 +0330

    student list

---
+++ Models/Student/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication7.Models.Student
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+}
